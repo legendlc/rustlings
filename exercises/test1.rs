e813f5c902da10f4a2563a5fc031241487321c67
Li Linchen <llc_legend@163.com>
2019-07-31 12:13:43 +0800

    first commit
    Signed-off-by: Li Linchen <llc_legend@163.com>

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,9 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculate_price(num: i32) -> i32 {
+    if num > 40 { num * 1 } else { num * 2 }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,6 +5,21 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+mod llc {
+    #[macro_export]
+    macro_rules! my_macro {
+        ($arg: expr) => {
+            {
+                let mut s = String::new();
+                s.push_str("Hello ");
+                s.push_str($arg);
+                s
+            }
+        };
+    }
+}
+
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
